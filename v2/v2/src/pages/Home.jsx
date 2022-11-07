deeb84dd9b3b1ccb95ef7497cd6cb20c111da614
StefanNistor69 <snistor147@gmail.com>
2022-11-07 15:16:34 +0200

    homePage

--- v2/v2/src/pages/Home.jsx
+++ v2/v2/src/pages/Home.jsx
@@ -1,9 +1,23 @@
 import '../MyStyle/Home.css';
+import React from "react";
 import ImageSlider from '../components/ImageSlider';
 import { SliderData } from '../components/SliderData';
+import { NavLink } from "react-router-dom";
+import { Button,Typography } from 'antd';
+const { Title, Text } = Typography;
 
-function Home() {
-  return ;
+
+
+
+const Home= ({}) => {
+
+  return (
+  <React.Fragment>
+  <Title style={{color:"white",marginTop:"400px",fontSize:"50px",fontWeight:"bold",fontFamily:"Helvetica"}}>Order your Trip!</Title>
+  <Title level={2} style={{color:"white",fontWeight:"normal",fontSize:"30px",fontFamily:"Helvetica"}}>Easy and free for all</Title>
+  <NavLink to="/login"><Button className='btn' shape="round" size='large'>Get Started</Button></NavLink>
+  </React.Fragment>
+  )
 }
 
 export default Home;
