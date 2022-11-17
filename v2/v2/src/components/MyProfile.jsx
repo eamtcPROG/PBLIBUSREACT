0aadbee5055ba4c51f3e18c61351b7e9090f9bae
Coretchi Mihai <prog.eamtc@gmail.com>
2022-11-17 15:17:27 +0200

    Profile update

--- v2/v2/src/components/MyProfile.jsx
+++ v2/v2/src/components/MyProfile.jsx
@@ -1,17 +1,18 @@
 
-import { Card, Space, Typography,Row,Col } from 'antd';
+import { Card, Space, Typography,Row,Col, Skeleton } from 'antd';
 import React, { useState, useEffect } from 'react';
 import { format } from 'date-fns'
 const MyProfile = () => {
     const [state, setState] = useState([]);
-  
+    const [userid, setUserId] = useState(0);
+    const [birthdate, setBirhdate] = useState();
     const [loading, setloading] = useState(true);
     
     const { Title, Text } = Typography;
     useEffect(() => {
       getData();
       console.log(state);
-    }, [loading]);
+    }, [loading,userid]);
     const getData = async () => {
       const token = localStorage.getItem('token');
       fetch(`http://localhost:8080/api/auth/getuser`, {
@@ -27,6 +28,9 @@ const MyProfile = () => {
   
         if (data) {
           setState(data);
+          setUserId(data.IdUser);
+          setBirhdate(data.Birthdate);
+          console.log(data);
           setloading(false);
         }
       })
@@ -67,7 +71,7 @@ const MyProfile = () => {
                     <Col span={4}>
                         Birthdate
                     </Col>
-                    <Col push={1}>{ format(new Date(state.Birthdate), 'dd-MM-yyyy')}</Col>
+                    {birthdate!= undefined?<Col push={1}>{ format(new Date(birthdate), 'dd-MM-yyyy')}</Col>:<Skeleton/>}
                 </Row>
 
 
