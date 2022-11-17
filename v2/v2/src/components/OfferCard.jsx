5e468e491e57b4f1063b8d6c82388ca656a3c10d
AlexGrama22 <alexgrama837@gmail.com>
2022-11-17 13:54:41 +0200

    Transporter Card

--- v2/v2/src/components/OfferCard.jsx
+++ v2/v2/src/components/OfferCard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Button, Card } from "antd";
+import { Button, Card,Row,Col } from "antd";
 import { EditOutlined } from '@ant-design/icons';
 import { useNavigate } from "react-router-dom";
 import Axios from "axios";
@@ -19,36 +19,40 @@ const OfferCard = ({ state, setOfferId }) => {
     return state != undefined ? (
         <>
 
-            <Card
-                style={{
-                    width: "45em",
-                    marginTop: 16,
-                }}
-                actions={[
-                    <Button onClick={handleMakeOffer}>Accept offer</Button>
-                ]}
-            >
-                <Card.Grid hoverable={false} style={{
-                    width: '33%',
-                    textAlign: 'center',
+            <Row>
+                <Col offset={1}>
+                            <Card
+                            style={{
+                                width: "15em",
+                                marginTop: 16,
+                            }}
+                            actions={[
+                                <Button onClick={handleMakeOffer}>Accept offer</Button>
+                            ]}
+                        >
+                            <Card.Grid hoverable={false} style={{
+                                width: '33%',
+                                textAlign: 'center',
 
-                }}>
+                            }}>
 
-                    {state != undefined ? state.Price : ""}
-                </Card.Grid>
-                <Card.Grid hoverable={false} style={{
-                    width: '33%',
-                    textAlign: 'center',
-                }}>
-                    {state != undefined ? state.OrderId : ""}
-                </Card.Grid>
-                <Card.Grid hoverable={false} style={{
-                    width: '34%',
-                    textAlign: 'center',
-                }}>
-                    {state != undefined ? state.TrasporterId : ""}
-                </Card.Grid>
-            </Card>
+                                {state != undefined ? state.Price : ""}
+                            </Card.Grid>
+                            <Card.Grid hoverable={false} style={{
+                                width: '33%',
+                                textAlign: 'center',
+                            }}>
+                                {state != undefined ? state.OrderId : ""}
+                            </Card.Grid>
+                            <Card.Grid hoverable={false} style={{
+                                width: '34%',
+                                textAlign: 'center',
+                            }}>
+                                {state != undefined ? state.TrasporterId : ""}
+                            </Card.Grid>
+                        </Card>
+                </Col>
+            </Row>
         </>
     ) : (<></>);
 };
