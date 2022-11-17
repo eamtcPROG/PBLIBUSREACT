07db0b79039d88ad7b3b49e039e682d526d6560a
Coretchi Mihai <prog.eamtc@gmail.com>
2022-11-17 14:57:40 +0200

    Profie

--- v2/v2/src/components/MyProfile.jsx
+++ v2/v2/src/components/MyProfile.jsx
@@ -1,6 +1,39 @@
-import { Card, Descriptions, Row, Col } from 'antd';
-import React from 'react';
-const MyProfile = () => (
+
+import { Card, Space, Typography,Row,Col } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { format } from 'date-fns'
+const MyProfile = () => {
+    const [state, setState] = useState([]);
+  
+    const [loading, setloading] = useState(true);
+    
+    const { Title, Text } = Typography;
+    useEffect(() => {
+      getData();
+      console.log(state);
+    }, [loading]);
+    const getData = async () => {
+      const token = localStorage.getItem('token');
+      fetch(`http://localhost:8080/api/auth/getuser`, {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          token: token,
+        },
+      }).then((res) => {
+        return res.json();
+  
+      }).then((data) => {
+  
+        if (data) {
+          setState(data);
+          setloading(false);
+        }
+      })
+        .catch(console.error);
+      
+    };
+    return(
     <div className="site-card-border-less-wrapper">
         <Card
             title="Profile Info"
@@ -12,25 +45,29 @@ const MyProfile = () => (
 
             
                 <Row>
-                    <Col>
-                        Nume
+                    <Col span={4}>
+                        <Text>Name</Text>
                     </Col>
+                    <Col push={1}>{state.Name}</Col>
                 </Row>
                 <Row>
-                    <Col>
-                       prenume
+                    <Col span={4}>
+                        <Text>Surname</Text>
                     </Col>
+                    <Col push={1}>{state.Surname}</Col>
                 </Row>
 
                 <Row>
-                    <Col>
-                        email
+                    <Col span={4}>
+                        Email
                     </Col>
+                    <Col push={1}>{state.Email}</Col>
                 </Row>
                 <Row>
-                    <Col>
-                        Age
+                    <Col span={4}>
+                        Birthdate
                     </Col>
+                    <Col push={1}>{ format(new Date(state.Birthdate), 'dd-MM-yyyy')}</Col>
                 </Row>
 
 
@@ -40,5 +77,6 @@ const MyProfile = () => (
 
         </Card>
     </div>
-);
+    )
+        }
 export default MyProfile;
