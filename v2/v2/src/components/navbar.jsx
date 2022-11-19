2fef2423d9cc92d99dbce6f22f86d521517503bd
eamtcPROG <prog.eamtc@gmail.com>
2022-11-20 01:37:15 +0200

    Navbar update

--- v2/v2/src/components/navbar.jsx
+++ v2/v2/src/components/navbar.jsx
@@ -1,4 +1,4 @@
-import { Form, Menu, Tabs, Button } from 'antd';
+import { Form, Menu, Tabs, Button, Col,Row } from 'antd';
 import React, { useEffect, useState } from 'react';
 import 'antd/dist/antd.css';
 import '../MyStyle/MyNavBarStyle.css'
@@ -30,12 +30,9 @@ const MyNavBar = ({ setIsAuthenticated, typeUserId, isAuthenticated }) => {
     }
 
     const [items, setItems] = useState([])
+    const [itemsSecond, setItemsSecond] = useState([])
     useEffect(() => {
         const token = localStorage.getItem('token');
-
-        if (token == null) {
-            setIsAuthenticated(false);
-        }
         fetch(`http://localhost:8080/api/auth/check-auth`, {
             headers: {
                 Accept: 'application/json',
@@ -45,74 +42,92 @@ const MyNavBar = ({ setIsAuthenticated, typeUserId, isAuthenticated }) => {
         }).then((res) => {
             if (res.status == 200) {
                 setIsAuthenticated(true);
+                setItems([
+                    typeUserId == 1 ? {
+    
+                        key: "offerpage",
+                        label: (<NavLink to="/offerpage">My Offer</NavLink>),
+                        icon: <FormOutlined />
+                    } : {
+    
+    
+                        key: "orderpage",
+                        label: (<NavLink to="/orderpage">My Order</NavLink>),
+                        icon: <TagOutlined />
+                    },
+    
+                    typeUserId == 1 ? {
+                        key: "Order",
+                        label: (<NavLink to="/order">Order</NavLink>),
+                        icon: <TagOutlined />
+                    }
+                        : null,
+                    typeUserId == 1 ? {
+                        key: "PersonalCabinetT",
+                        label: (<NavLink to="/transporter">My Profile</NavLink>),
+                        icon: <UserOutlined />
+                    }
+                        : {
+                            key: "PersonalCabinetC",
+                            label: (<NavLink to="/customer">My Profile</NavLink>),
+                            icon: <UserOutlined />
+                        }
+                ])
+                
+                setItemsSecond([
+                    {
+                        key: "Logout",
+                        label: (<Button type="text" className='logoutbtn' icon={<PoweroffOutlined />} block={true} onClick={logout}
+                        > Logout</Button>),
+                    }])
             }
             else {
                 setIsAuthenticated(false);
+                setItems([]);
+                setItemsSecond([{
+
+                    key: "Login",
+                    label: (<NavLink align="middle" to="/login">Sign in</NavLink>),
+                    icon: <FormOutlined />
+                }])
             }
 
         })
             .catch(console.error);
         console.log(isAuthenticated)
-        if (isAuthenticated == true) {
-            setItems([
-                typeUserId == 1 ? {
+        
 
-                    key: "offerpage",
-                    label: (<NavLink to="/offerpage">My Offer</NavLink>),
-                    icon: <FormOutlined />
-                } : {
-
-
-                    key: "orderpage",
-                    label: (<NavLink to="/orderpage">My Order</NavLink>),
-                    icon: <TagOutlined />
-                },
-
-                typeUserId == 1 ? {
-                    key: "Order",
-                    label: (<NavLink to="/order">Order</NavLink>),
-                    icon: <TagOutlined />
-                }
-                    : null,
-                typeUserId == 1 ? {
-                    key: "PersonalCabinetT",
-                    label: (<NavLink to="/transporter">My Profile</NavLink>),
-                    icon: <UserOutlined />
-                }
-                    : {
-                        key: "PersonalCabinetC",
-                        label: (<NavLink to="/customer">My Profile</NavLink>),
-                        icon: <UserOutlined />
-                    },
-                {
-                    key: "Logout",
-                    label: (<Button type="text" className='logoutbtn' icon={<PoweroffOutlined />} block={true} onClick={logout}
-                    > Logout</Button>),
-                }
-            ])
-        } else {
-            setItems([{
-
-                key: "OfferPage",
-                label: (<NavLink align="middle" to="/login">Sign in</NavLink>),
-                icon: <FormOutlined />
-            }])
-        }
     }, [isAuthenticated])
 
+
+
+
     return (
-        <>
-            <div className="logo" />
-            <Menu
-                theme="light"
-                mode="horizontal"
-                defaultSelectedKeys={"OfferPage"}
-                items={items}
-            >
 
-            </Menu>
+        <Row wrap={false}>
+            <Col xs={2} sm={2} md={2} lg={2} xl={2}>
+                <div className="logo" />
+            </Col>
+            <Col xs={14} sm={13} md={15} lg={17} xl={18}>
+                <Menu
+                    theme="light"
+                    mode="horizontal"
+                    defaultSelectedKeys={"OfferPage"}
+                    items={items}
+                />
+            </Col>
+            <Col  xs={8} sm={9} md={7} lg={5} xl={4}>
+                <Menu
+                    style={{marginLeft:"20%"}}
+                    theme="light"
+                    mode="horizontal"
+                    defaultSelectedKeys={"OfferPage"}
+                    items={itemsSecond}
+                />
+            </Col>
+        </Row>
 
-        </>
+
     );
 }
 
